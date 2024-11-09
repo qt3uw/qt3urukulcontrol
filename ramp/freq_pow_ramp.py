e85eb9f608de3095ac82ad57a78ee1424e1c009d
alcj3 <a.lopez115@outlook.com>
2024-11-09 14:27:24 -0800

    created a class to test for power and frequency ramping

---
+++ ramp/freq_pow_ramp.py
@@ -0,0 +1,37 @@
+from artiq.experiment import *
+
+class RampControl:
+    def __init__(self):
+        # freq ramp parameters
+        self.start_freq = 5 * MHz
+        self.end_freq = 50 * MHz
+        self.ramp_duration = 100 * ms
+        self.num_steps = 1000
+        self.step_duration = self.ramp_duration / self.num_steps
+        self.freq_step = (self.end_freq - self.start_freq) / self.num_steps
+
+        # amplitude parameters
+        self.start_amplitude = 0.1           
+        self.end_amplitude = 0.8             
+        self.amp_step = (self.end_amplitude - self.start_amplitude) / self.num_steps
+
+
+    @kernel
+    def run_frequency_ramp(self, channel):
+        current_freq = self.start_freq
+
+        for step in range(self.num_steps):
+            channel.set(current_freq)
+            channel.cpld.io_update.pulse_mu(8)
+            delay(self.step_duration)
+            current_freq += self.freq_step
+
+    @kernel
+    def run_amplitude_ramp(self, channel):
+        current_amplitude = self.start_amplitude
+        
+        for step in range(self.num_steps):
+            channel.set(amplitude =current_amplitude)
+            channel.cpld.io_update.pulse_mu(8)
+            delay(self.step_duration)
+            current_amplitude += self.amp_step
---
+++ ramp/sample_exp.py
@@ -0,0 +1,28 @@
+from artiq.experiment import *
+from freq_pow_ramp import RampControl
+
+class SampleExperiment(EnvExperiment):
+    def build(self):
+        self.setattr_device("core")
+        self.setattr_device("urukul0_ch1")
+        self.ramp = RampControl()
+
+    @kernel
+    def run(self):
+        # Reset core device and initialize CPLD and DDS on channel 1
+        self.core.reset()
+        self.urukul0_ch1.cpld.init()        
+        self.urukul0_ch1.init()             
+        delay(10 * ms)                      
+
+        # Set initial attenuation and turn on the switch
+        attenuation = 6.0 * dB
+        self.urukul0_ch1.set_att(attenuation)
+        self.urukul0_ch1.sw.on()
+
+        # Run frequency and amplitude ramping
+        self.ramp.run_frequency_ramp(self.urukul0_ch1)
+        self.ramp.run_amplitude_ramp(self.urukul0_ch1)
+
+        # Turn off the DDS channel after completing the ramp
+        self.urukul0_ch1.sw.off()
